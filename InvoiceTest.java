3103dcde81e1e8d0d35da241a4c2a9ae2f4c7d22
Edward M <edwardtmuniz@gmail.com>
2020-02-16 21:30:28 -0700

    turning this in

--- InvoiceTest.java
+++ InvoiceTest.java
@@ -10,38 +10,29 @@ package app;
 
 import java.util.Scanner;
 
-public class App {
+public class InvoiceTest {
     public static void main(String[] args) throws Exception {
-        // setup the input
-        Scanner input = new Scanner(System.in);
-
         // introduce the program
         System.out.println();
         System.out.print("Hello! The purpose of this program is to use an  ");
         System.out.print("Invoice class with useful attributes and methods \n");
 
-        // get user input
-        System.out.print("Type a model number for your part: ");
-        String partNumber = input.nextLine();
-        System.out.print("Type a description for this part: ");
-        String partDesc = input.nextLine();
-        System.out.print("Type how many of this part are in stock: ");
-        Integer partQty = input.nextInt();
-        System.out.print("Type a price for your part: ");
-        Double partPrice = input.nextDouble();
-
         // create a new instance
-        Invoice invoice = new Invoice(partNumber, partDesc, partQty, partPrice);
+        Invoice invoice = new Invoice(null, null, 0, 0.0);
+
+        // invoke input method for user input
+        input(invoice);
 
         // output for the user
         System.out.println();
         output(invoice);
+    }
 
-        // making the user do it again for the sake of demonstration
-        System.out.println();
-        System.out.print("For demonstration purposes, we're going to do all this again.\n");
+    public static void input(Invoice invoice) {
+        // setup the input
+        Scanner input = new Scanner(System.in);
 
-        // demonstrate setters
+        // get user via Scanner
         System.out.print("Type a model number for your part: ");
         invoice.setPartNumber(input.nextLine());
         System.out.print("Type a description for this part: ");
@@ -50,19 +41,14 @@ public class App {
         invoice.setPartQty(input.nextInt());
         System.out.print("Type a price for your part: ");
         invoice.setPartPrice(input.nextDouble());
-
         input.close();
-
-        // output for the user AGAIN
-        System.out.println();
-        output(invoice);
     }
 
     public static void output(Invoice invoice) {
         System.out.println("Part number: " + invoice.getPartNumber());
         System.out.println("Description: " + invoice.getPartDesc());
         System.out.println("Quantity: " + invoice.getPartQty());
-        System.out.println("Price: " + invoice.getPartPrice());
+        System.out.println("Price: $" + invoice.getPartPrice());
         System.out.println("Total: $" + invoice.getInvoiceAmount());
     }
 
